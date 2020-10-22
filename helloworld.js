4e1b0d609589fb34d5be6414dfec419d73e741b5
Ling zhidong <46412564+Lexus-D@users.noreply.github.com>
2020-10-22 15:26:29 +0900

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
