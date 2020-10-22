d441a6402a28f6348c7180b4398cfcdff7f2dbf3
Ling zhidong <46412564+Lexus-D@users.noreply.github.com>
2020-10-22 16:13:04 +0900

    a

--- helloworld.js
+++ helloworld.js
@@ -3,7 +3,7 @@ var dt=require("./myfirstmodule");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("print the current date: "+dt.myDateTime);
+  res.write("print the current date: "+dt.myDateTime()+"\n");
   res.end('Hello World!');
 }).listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
