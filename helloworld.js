329456afe51496d6d05cc41e702b008f0241dd30
Ling zhidong <46412564+Lexus-D@users.noreply.github.com>
2020-10-22 16:04:12 +0900

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(8080);
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:8080/');
