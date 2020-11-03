44799effb5ec1de91f4569594bbcd64c81ae68b5
Ling zhidong <46412564+Lexus-D@users.noreply.github.com>
2020-11-04 05:30:51 +0900

    l

--- 5_installpackage.js
+++ 5_installpackage.js
@@ -1,9 +1,9 @@
 /*
 1.Install package from npm
 2.Require installed package from directory
-  npm install md5 --save(write necessary package info into package.json)
-3.Use method
+  npm install md5 --save(write necessary packages info into package.json)
+  for newly downloaded projet, use 'npm i' to install all necessary packages
+3.Use method while reading document
 */
 const md5 = require("md5");
-
 console.log(md5('heros never die!'));
---
+++ sillydatetime.js
@@ -0,0 +1,4 @@
+var sd=require('silly-datetime');
+
+
+console.log(sd.format(new Date(), 'YYYY-MM-DD HH:mm'));
