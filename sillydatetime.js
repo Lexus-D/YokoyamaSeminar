296f5a8a8a926206c203b16942db7844be4a7acb
Ling zhidong <46412564+Lexus-D@users.noreply.github.com>
2020-11-04 06:16:03 +0900

    あ

--- sillydatetime.js
+++ sillydatetime.js
@@ -1,4 +1,6 @@
-var sd=require('silly-datetime');
-
+const sd=require('silly-datetime');
+const md5 = require("md5");
 
 console.log(sd.format(new Date(), 'YYYY-MM-DD HH:mm'));
+
+console.log(md5(sd.format(new Date(), 'YYYY-MM-DD HH:mm')));
